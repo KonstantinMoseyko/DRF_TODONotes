85b9d41a99d477e331f0e1631fe648228f2293f9
KonstantinMoseyko <90209282+KonstantinMoseyko@users.noreply.github.com>
2022-08-20 21:13:33 +0300

    lesson_4 Finished (#3)
    lesson_4 Finished

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,14 +1,32 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
+from .filters import ProjectFilter, TodoFilter
 from .models import Project, Todo
 from .serializers import ProjectModelSerializer, TodoModelSerializer
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     serializer_class = ProjectModelSerializer
     queryset = Project.objects.all()
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TodoModelViewSet(ModelViewSet):
     serializer_class = TodoModelSerializer
     queryset = Todo.objects.all()
+    pagination_class = TodoLimitOffsetPagination
+    filterset_class = TodoFilter
+
+    def perform_destroy(self, instance):
+        instance.status = True
+        instance.save()
