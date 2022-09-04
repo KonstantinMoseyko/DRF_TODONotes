6ad62c69ccb19483033bd72055a15faa646fb066
KonstantinMoseyko <mosey577@mail.ru>
2022-09-04 13:49:03 +0300

    lesson_9 Finished

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from todoapp.views import ProjectModelViewSet, TodoModelViewSet
 from users.views import UserModelLimitedViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TodoApp",
+        default_version="1.0",
+        description="Documentation to out project",
+        contact=openapi.Contact(email="admin@admin.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+)
 
 router = DefaultRouter()
 # router.register("users", UserModelViewSet)
@@ -33,4 +45,6 @@ urlpatterns = [
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
     path("api-token-auth/", views.obtain_auth_token),
+    path("swagger", schema_view.with_ui()),
+    re_path(r"swagger(?P<format>\.json|\.yaml)", schema_view.without_ui()),
 ]
